8697304e5dca0dd94e3beddf1858ed97b48b67df
TorenUK <toren@toren.uk>
2025-10-14 18:16:58 +0100

    chore: update firebase config

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDwxrBtc9xKA_zWu_zZtbzcqPj0A67RbLc",
   authDomain: "local-app-a5298.firebaseapp.com",
@@ -18,4 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 const analytics = getAnalytics(app);
